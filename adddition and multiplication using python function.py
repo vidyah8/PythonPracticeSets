238519c7241a43045184419fbc06eb94438cc9c5
vidyah8 <75680346+vidyah8@users.noreply.github.com>
2021-05-28 19:34:49 +0530

    Add files via upload

---
+++ adddition and multiplication using python function.py
@@ -0,0 +1,9 @@
+def add(numbers):
+    sum_of_numbers = sum(numbers)
+    return sum_of_numbers
+numbers=[20,30]
+sum_of_numbers = add(numbers)
+print("addtion of two numbers:",sum_of_numbers)
+# addtion of two numbers using python function sum
+
+#multiplication of two numbers
---
+++ addingntwo numbers.py
@@ -0,0 +1,4 @@
+num1 = input("first number : ")
+num2 = input("\n second number : ")
+sum = num1+num2
+print("the sum of two numbers : " .format (num1,num2,sum))
